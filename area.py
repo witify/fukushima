e5f73c72219cd45fb3a9531e937fdd2d3e809fa1
François Lévesque <francois@witify.io>
2023-06-21 12:19:35 -0400

    Fixes

--- area.py
+++ area.py
@@ -1,43 +1,40 @@
 import geopandas as gpd
 import utm
 
-fukushima = gpd.read_file('data/fukushima.geojson')
-# fukushima.crs = 'epsg:900913'
-# fukushima = fukushima.to_crs(crs=3857)
+fukushima = gpd.read_file('data/fukushima-2.geojson')
+
+projection = '+proj=cea +lat_0=35.68250088833567 +lon_0=139.7671 +units=m'
+
+fukushima = fukushima.to_crs(projection)
 
 waste2022 = gpd.read_file('data/raw/geojson/Waste_2022.geojson')
 waste2023 = gpd.read_file('data/raw/geojson/Waste_2023.geojson')
 solar2022 = gpd.read_file('data/raw/geojson/Solar_2022.geojson')
 solar2023 = gpd.read_file('data/raw/geojson/Solar_2023.geojson')
-# waste2022.crs = 'epsg:4326'
-# waste2022 = waste2022.to_crs(crs=3857)
+
+waste2022 = waste2022.to_crs(projection)
+waste2023 = waste2023.to_crs(projection)
+solar2022 = solar2022.to_crs(projection)
+solar2023 = solar2023.to_crs(projection)
 
 
 for index, city in fukushima.iterrows():
     city = fukushima.iloc[[index]]
-
-    # print(city.ward_en.to_string(index=False), city.geometry.area.sum() * 10000)
-
-    # city = city.to_crs(crs=3857)
 
     interWaste2022 = gpd.overlay(city, waste2022, how='intersection')
     interWaste2023 = gpd.overlay(city, waste2023, how='intersection')
     interSolar2022 = gpd.overlay(city, solar2022, how='intersection')
     interSolar2023 = gpd.overlay(city, solar2023, how='intersection')
 
-    coef = 1000000 * 1239.2026121119  # convert to m^2
-
-    fukushima.at[index, 'area'] = city.geometry.area.sum() * coef
-    fukushima.at[index, 'waste2022'] = interWaste2022.geometry.area.sum() * \
-        coef
-    fukushima.at[index, 'waste2023'] = interWaste2023.geometry.area.sum() * \
-        coef
-    fukushima.at[index, 'solar2022'] = interSolar2022.geometry.area.sum() * \
-        coef
-    fukushima.at[index, 'solar2023'] = interSolar2023.geometry.area.sum() * \
-        coef
+    fukushima.at[index, 'area'] = city.geometry.area.sum()
+    fukushima.at[index, 'waste2022'] = interWaste2022.geometry.area.sum()
+    fukushima.at[index, 'waste2023'] = interWaste2023.geometry.area.sum()
+    fukushima.at[index, 'solar2022'] = interSolar2022.geometry.area.sum()
+    fukushima.at[index, 'solar2023'] = interSolar2023.geometry.area.sum()
     fukushima.at[index, 'intersection'] = (fukushima.iloc[index].solar2023 - fukushima.iloc[index].solar2022) + (
         fukushima.iloc[index].waste2022 - fukushima.iloc[index].waste2023)
 
+
+fukushima = fukushima.to_crs('4326')
 
 fukushima.to_file('data/generated/fukushima.geojson', driver='GeoJSON')
